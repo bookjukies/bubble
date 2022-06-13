5e22791cab99b197c9173036c0caacc18a11d64e
Bongani <mkhumbie34@gmail.com>
2022-06-13 02:11:21 +0200

    final commit

--- assets/scripts/bubble.js
+++ assets/scripts/bubble.js
@@ -1,8 +1,4 @@
-  //creating the class for the custom element
-  /*
-    The all the sorces(src) must point to index pages reletive position
-    if you choose to move the videos and the icons youll have to use the path in relation to the index page
-  */
+
   class MyBubble extends HTMLElement {
       connectedCallback(){
           this.innerHTML=`
@@ -15,13 +11,13 @@
                 <!-- VIDEO CONTROLS -->
                     <div class="controls_ ">
                         <div class="left_">
-                        
-                            <img id="pause_" class="icon_" src="./pause.png" alt="pause">
-                            <img id="play_" class="icon_ none_" src="./play.png" alt="play">
-                            <img id="refresh_" class=" icon_" src="./refresh.png" alt="refresh">
-                            <img id="volume_" class=" icon_ none_" src="./volume.png" alt="volume">
-                            <img id="mute_" class=" icon_ " src="./mute.png" alt="mute">
-                            <img id="close_" class="icon_" src="./close.png" alt="close">
+                                                    
+                            <img id="pause_" class="icon_" src="./assets/icons/pause.png" alt="pause">
+                            <img id="play_" class="icon_ none_" src="./assets/icons/play.png" alt="play">
+                            <img id="refresh_" class=" icon_" src="./assets/icons/refresh.png" alt="refresh">
+                            <img id="volume_" class=" icon_ none_" src="./assets/icons/volume.png" alt="volume">
+                            <img id="mute_" class=" icon_ " src="./assets/icons/mute.png" alt="mute">
+                            <img id="close_" class="icon_" src="./assets/icons/close.png" alt="close">
                         </div>
                     </div>
                 <!-- CONTROLS END -->
@@ -29,12 +25,12 @@
 
                 <!-- VIDEO SORCER -->
                     <video muted autoplay loop class="video_ open_">
-                        <source src="./test.mp4" type="video/mp4" />
+                        <source src="./assets/videos/test.mp4" type="video/mp4" />
                     </video>
                 <!--VIDEO SORCE END -->
                    
                 <!--CALL TO ACTION BUTTON-->
-                    <a class="started_ down_" href="#"><span><img class="shuffle_" src="./shuffle.png" alt="get started">GET
+                    <a class="started_ down_" href="#"><span><img class="shuffle_" src="./assets/icons/shuffle.png" alt="get started">GET
                             STARTED</span></a>
                 </div>
                 <!--CALL TO ACTION BUTTON... END--> 
@@ -49,11 +45,11 @@
 
         <!--VIDEO SOURCR-->
                         <video muted autoplay loop class="video_ circle_ fixed_">
-                            <source src="./test.mp4" type="video/mp4" />
+                            <source src="./assets/videos/test.mp4" type="video/mp4" />
                         </video>
 
         <!--CLOSE BUTTON-->
-                    <div class="close_ fixed_"><img class="close_btn_" src="./outer.png" alt="close"></div>
+                    <div class="close_ fixed_"><img class="close_btn_" src="./assets/icons/outer.png" alt="close"></div>
                  <!--CLOSE BUTTON END-->
                 </div>
             </div>
